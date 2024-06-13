26636c5a3d95946f14f629cf9dbc61aac9f65ba3
amfad33 <afadaei2@gmail.com>
2024-06-13 14:11:29 +0330

    Update audio_stream.py

--- models/audio_stream.py
+++ models/audio_stream.py
@@ -8,20 +8,17 @@ class FullyConnectedAudioNet(nn.Module):
         super(FullyConnectedAudioNet, self).__init__()
         self.fc1 = nn.Linear(input_size, 1024)
         self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 1024)
-        self.nextvlad = nv.NeXtVLAD(dim=1024, num_clusters=32, lamb=2, groups=16, max_frames=frame_count)
-        self.fc4 = nn.Linear(4096, c)
+        self.nextvlad = nv.NeXtVLAD(dim=512, num_clusters=32, lamb=2, groups=16, max_frames=frame_count)
+        self.fc3 = nn.Linear(2048, c)
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
         nn.init.xavier_uniform_(self.fc3.weight)
-        nn.init.xavier_uniform_(self.fc4.weight)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
         x = self.nextvlad(x)
-        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc3(x))
         return x
 
 # Example usage:
